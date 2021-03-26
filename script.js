2e8ca694d12d6902ef1b812df4cbb0d2c87f30c2
Brian-wKicheu <78160253+Brian-wKicheu@users.noreply.github.com>
2021-03-26 20:39:44 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,128 +1,28 @@
 //alert("Wewe hello, there?")
 //console.log ("alafu niaje morio unajikuna kuna")
 
-//variable syntax
-//var name="value"
-
-//
-//3types of initialization
-//
-//1. var -pure global
-//2. let-can be pure or global
-//3. const- defines that doesnt change; e.g pie.
-//
-
-//prac 1
-//const animal="dog"
-//  function greeting(){
-//     const animal="cat"
-//      console.log(animal);
-//  }
-//greeting()
-//console.log(animal)
-
-//prac 2 prac 3 should return 2nd name
-// var name="Felix"
-// var name="Oginda"
-// console.log(name)
-
-//prac 3 should return 2nd name
-//let name="Felix"
-//    name="Oginda"
-//console.log(name)
-
-//prac 4 constant is only defined once  
-//const PIE=3.142
-//    
-//console.log(PIE)
-
-
-//naming rules
-//1. name case
-//i. var myName="felo"
-//ii. var ourStudentAge="23"
-//ii. var ourStudentAge="23"
-// 
-
-
-//4. data types
-//1. integer
-//
-//var age="2202.56"
-//
-//2. String
-//var grade="A"
-//var name="Felix"
-//
-//3. undifined ( variable that has not been defined)
-//
-//4. null (variable that has no value)
-//5. Boolean
-//
-//var islogin="true"
-//var islogin="false"
-//
-
-
-//operators +,-,/,%
-//var a=30
-//var b=3
-//var sum=a+b
-//var sub=a-b
-//var mult=a*b
-//var div=a/b
-//var modulus=a%b
-//
-//console.log(sum)
-//console.log(sub)
-//console.log(mult)
-//console.log(div)
-//console.log(modulus)
-
-//project calculator
-
-var num1= prompt("enter number1")
-var num2= prompt("enter number2")
-var sum= Number(num1)+Number(num2)
-var myResult=document.createElement('h1')
-myResult.innerHTML=sum;
-document.body.append(myResult)
-
-//var num1= prompt("enter number1")
-//var num2= prompt("enter number2")
-//var sub= Number(num1)-Number(num2)
-//console.log(sub)
-
-//var num1= prompt("enter number1")
-//var num2= prompt("enter number2")
-//var mult= Number(num1)*Number(num2)
-//console.log(mult)
-//
-//var num1= prompt("enter number1")
-//var num2= prompt("enter number2")
-//var div= Number(num1)/Number(num2)
-//console.log(div)
-//
-//var num1= prompt("enter number1")
-//var num2= prompt("enter number2")
-//var modulus= Number(num1)%Number(num2)
-//console.log(modulus)
 
 
 
+//simple-server-in-nodejs.js 						
+var http = require('http');
 
+var host = '127.0.0.1'
+var port = 3000
 
+var server = http.createServer((request, response) => {
+  response.writeHead(200, {"Content-Type": "text/plain"});
+  console.log("server Working");
+  response.end('Server Working Success');
+});
 
+server.listen(port,host, (error) => {  
+  if (error) {
+    return console.log('Error occured : ', error );
+  }
 
-
-
-
-
-
-
-
-
-
+  console.log('server is listening on ' + host + ':'+ port);
+});
 
 
 
